832d092f5e46b653d4518c9345193a3a1c44a6f6
radek-winiecki <winiecki.radoslaw@gmail.com>
2022-02-21 12:41:07 +0100

    create a email input in the register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,6 +4,22 @@
     <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
+
+    <!-- Register -->
+    <form class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+      <h1 class="text-3xl text-at-light-green mb-4">Rejestracja</h1>
+
+      <div class="flex flex-col mb-2">
+        <label for="email" class="mb-1 text-sm text-at-light-green">E-Mail:</label>
+        <input 
+          type="text" 
+          required 
+          class="p-2 text-gray-500 focus:outline-none" 
+          id="email"
+          v-model="email"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
