53f6c917fc01ca6eb68316260e040577e7a7b914
radek-winiecki <winiecki.radoslaw@gmail.com>
2022-02-22 12:35:13 +0100

    create a greeting, label and input for the workout type

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,9 +1,34 @@
 <template>
   <div class="max-w-screen-md mx-auto px-4 py-10">
     <!-- Status Message -->
-    <div v-if="statusMsg || errorMsg" class="mb-10 p-4 bg-light-grey rounded-md">
+    <div v-if="statusMsg || errorMsg" class="mb-10 p-4 bg-light-grey rounded-md shadow-lg">
       <p class="text-at-light-green">{{ statusMsg }}</p>
       <p class="text-red-500">{{ errorMsg }}</p>
+    </div>
+
+    <!-- Create -->
+    <div class="p-8 flex items-start bg-light-grey rounded-md shadow-lg">
+      <!-- Form -->
+      <form class="flex flex-col gap-y-5 w-full">
+
+        <h1 class="text-2xl text-at-light-green">Privet!</h1>
+
+        <!-- Workout Name -->
+        <div class="flex flex-col">
+          <label 
+          for="workout-name" 
+          class="mb-1 text-sm text-at-light-green">jaki trening, Byczku?
+          </label>
+          
+          <input
+           type="text" 
+           required 
+           class="p-2 text-grey-500 focus:outline-none" 
+           id="workout-name" 
+           v-model="workoutName"
+           >
+        </div>
+      </form>
     </div>
   </div>
 </template>
