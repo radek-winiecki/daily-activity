421db078b19a61f75589812f2a80d5febdfd1534
radek-winiecki <winiecki.radoslaw@gmail.com>
2022-02-23 15:04:19 +0100

    create a no-data situation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div v-if="dataLoaded" class="container mt-10 px-4">
+    
+    <!-- No Data -->
+    <div v-if="data.length === 0" class="w-full flex flex-col items-center">
+      <h1 class="text-2xl">Jeszcze nic tutaj nie ma...</h1>
+      <router-link
+        class="mt-20 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid
+               border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+       :to="{ name: 'create' }">Stwórz Trening</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
