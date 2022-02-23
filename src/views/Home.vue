c7ac6e7d6902b563cd456cd7ccb2b2c573c1c179
radek-winiecki <winiecki.radoslaw@gmail.com>
2022-02-23 13:36:04 +0100

    retrieve all data from database

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,33 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import { supabase } from "../supabase/init";
+
 export default {
   name: "Home",
   components: {},
   setup() {
     // Create data / vars
+    const data = ref([]);
+    const dataLoaded = ref(null);
 
     // Get data
+    const getData = async () => {
+      try {
+        const { data: workouts, error } = await supabase.from("workouts").select("*");
+        if (error) throw error;
+        data.value = workouts;
+        dataLoaded.value = true;
+        console.log(data.value);
+      }
+      catch(error) {
+        console.warn(error.message);
+      }
+    };
 
     // Run data function
+    getData();
 
     return {};
   },
