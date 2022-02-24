ab8b7bab376d41403553116da9aa310be9cf26b2
radek-winiecki <winiecki.radoslaw@gmail.com>
2022-02-24 21:02:38 +0100

    create a page / view titles

--- src/router/index.js
+++ src/router/index.js
@@ -10,26 +10,41 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    meta: {
+      title: "Login",
+    },
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
+    meta: {
+      title: "Register",
+    },
   },
   {
     path: "/create",
     name: "Create",
     component: Create,
+    meta: {
+      title: "Create",
+    },
   },
   {
     path: "/view-workout/:workoutId",
     name: "View-Workout",
     component: ViewWorkout,
+    meta: {
+      title: "View Workout"
+    },
   },
 ];
 
@@ -39,6 +54,10 @@ const router = createRouter({
 });
 
 // Change document titles
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Active Tracker`;
+  next();
+});
 
 // Route guard for auth routes
 
